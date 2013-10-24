69a9d6c5ffffccb49b9c58d23e40e19237c7473a
Syntox32 <syntox32@gmail.com>
2013-10-24 16:32:37 +0200

    Inital commit

--- WallbaseDownloader/MainWindow.xaml.cs
+++ WallbaseDownloader/MainWindow.xaml.cs
@@ -124,7 +124,6 @@ namespace WallbaseDownloader
             }
             else
             {
-                // Work on this
                 LockGUI(false);
                 Download();
             }
