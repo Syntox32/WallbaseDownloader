e2be2fbab51421fa8e26fb39250043e72629f429
Syntox32 <syntox32@gmail.com>
2013-10-26 15:04:00 +0200

    Fixed settings, now saves properly

--- WallbaseDownloader/Settings.xaml.cs
+++ WallbaseDownloader/Settings.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WallbaseDownloader
 {
@@ -23,7 +11,6 @@ namespace WallbaseDownloader
         {
             InitializeComponent();
 
-            checkAsync.IsChecked = UseAsync;
             checkCreateLog.IsChecked = CreateLog;
             checkCreateUrl.IsChecked = CreateList;
             checkUseDate.IsChecked = NameFolderWithDate;
@@ -33,31 +20,66 @@ namespace WallbaseDownloader
         public bool NameFolderWithDate
         {
             get { return Properties.Settings.Default.nameDate; }
-            set { Properties.Settings.Default.nameDate = value; Properties.Settings.Default.Save(); }
+            set
+            {
+                Properties.Settings.Default.nameDate = value;
+                Properties.Settings.Default.Save();
+            }
         }
 
         public bool CreateList
         {
             get { return Properties.Settings.Default.createList; }
-            set { Properties.Settings.Default.nameDate = value; Properties.Settings.Default.Save(); }
+            set
+            {
+                Properties.Settings.Default.createList = value;
+                Properties.Settings.Default.Save();
+            }
         }
 
         public bool CreateLog
         {
             get { return Properties.Settings.Default.createLog; }
-            set { Properties.Settings.Default.createLog = value; Properties.Settings.Default.Save(); }
-        }
-
-        public bool UseAsync
-        {
-            get { return Properties.Settings.Default.useAsync; }
-            set { Properties.Settings.Default.useAsync = value; Properties.Settings.Default.Save(); }
+            set
+            {
+                Properties.Settings.Default.createLog = value;
+                Properties.Settings.Default.Save();
+            }
         }
 
         public bool Sort
         {
             get { return Properties.Settings.Default.sort; }
-            set { Properties.Settings.Default.sort = value; Properties.Settings.Default.Save(); }
+            set
+            {
+                Properties.Settings.Default.sort = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void checkUseDate_Checked(object sender, RoutedEventArgs e)
+        {
+            NameFolderWithDate = (bool)checkUseDate.IsChecked;
+        }
+
+        private void checkSort_Checked(object sender, RoutedEventArgs e)
+        {
+            Sort = (bool)checkSort.IsChecked;
+        }
+
+        private void checkCreateUrl_Checked(object sender, RoutedEventArgs e)
+        {
+            CreateList = (bool)checkCreateUrl.IsChecked;
+        }
+
+        private void checkCreateLog_Checked(object sender, RoutedEventArgs e)
+        {
+            CreateLog = (bool)checkCreateLog.IsChecked;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
